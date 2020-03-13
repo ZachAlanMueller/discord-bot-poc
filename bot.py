0ce3997b3fbf26216e0d70ea9279b449a7415981
Zaedonn <zach.a.mueller@gmail.com>
2020-03-13 12:39:05 -0500

    small cleanup, slightly better verbage, setting up for background use

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
 import os
 import random
 import bisect
+
+import weightedChance #custom
 
 import discord
 from dotenv import load_dotenv
@@ -9,49 +13,14 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
-
-class WeightedChoice(object):
-    def __init__(self, weights):
-        self.totals = []
-        self.weights = weights
-        running_total = 0
-
-        for w in weights:
-            running_total += w[1]
-            self.totals.append(running_total)
-
-    def get(self):
-        rnd = random.random() * self.totals[-1]
-        i = bisect.bisect_right(self.totals, rnd)
-        return self.weights[i][0]
-
-
-maps = (
-    ('Customs', 15), 
-    ('Woods', 10), 
-    ('Shoreline', 2), 
-    ('Interchange', 20), 
-    ('Reserve', 30), 
-    ('Labs', 20)
-)
-times = (
-    ('night', 10),
-    ('day', 40)
-)
-playstyles = (
-    ('chad it up!', 30),
-    ('rat all that loot!', 10),
-    ('snipe!', 10),
-    ('be some thicc bois!', 30),
-    ('take some thermals!', 10),
-    ('do some quests!', 10),
-    ('pistol whip some scavs!', 1),
-    ('scav!', 5)
-)
-mapChoice = WeightedChoice(maps)
-playstyleChoice = WeightedChoice(playstyles)
-timeChoice = WeightedChoice(times)
+pres = (('Next you should go to ', 1), ('This run you should do ', 1), ('Maybe this time go to ', 1), ('Man up and go to ', 1), ('It\'s time to do ', 1))
+maps = (('Customs', 15), ('Woods', 10), ('Shoreline', 2), ('Interchange', 20), ('Reserve', 30), ('Labs', 20))
+times = (('night', 10),('day', 40))
+playstyles = (('chad it up!', 30),('rat all that loot!', 10),('snipe!', 10),('be some thicc bois!', 30),('take some thermals!', 10),('do some quests!', 10),('pistol whip some scavs!', 1),('scav!', 5))
+mapChoice = weightedChance.WeightedChoice(maps)
+playstyleChoice = weightedChance.WeightedChoice(playstyles)
+timeChoice = weightedChance.WeightedChoice(times)
+preChoice = weightedChance.WeightedChoice(pres)
 
 @client.event
 async def on_message(message):
@@ -68,7 +37,8 @@ async def on_message(message):
         nextMap = mapChoice.get()
         nextTime = timeChoice.get()
         nextStyle = playstyleChoice.get()
-        msg = ('Next you should go to '+nextMap + ' during the '+ nextTime + ' and '+ nextStyle).format(message)
+        nextPre = preChoice.get()
+        msg = (nextPre + nextMap + ' during the '+ nextTime + ' and '+ nextStyle).format(message)
         channel = message.channel
         await channel.send(msg)
 
@@ -80,3 +50,15 @@ async def on_ready():
     print('------')
 
 client.run(TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ weightedChance.py
@@ -0,0 +1,15 @@
+class WeightedChoice(object):
+    def __init__(self, weights):
+        self.totals = []
+        self.weights = weights
+        running_total = 0
+
+        for w in weights:
+            running_total += w[1]
+            self.totals.append(running_total)
+
+    def get(self):
+        rnd = random.random() * self.totals[-1]
+        i = bisect.bisect_right(self.totals, rnd)
+        return self.weights[i][0]
+
